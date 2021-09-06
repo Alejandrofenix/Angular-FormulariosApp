0a17d3f7b76d17672feed043520f81111c2768ec
Alejandro Pinedo B <apinedobarrios@live.com>
2021-09-06 13:29:51 -0500

    Se agregarón los omponentes y rutas a template

--- src/app/template/template-routing.module.ts
+++ src/app/template/template-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BasicosComponent } from '../reactive/basicos/basicos.component';
-import { DinamicosComponent } from '../reactive/dinamicos/dinamicos.component';
-import { SwitchesComponent } from '../reactive/switches/switches.component';
+import { BasicosComponent } from './basicos/basicos.component';
+import { DinamicosComponent } from './dinamicos/dinamicos.component';
+import { SwitchesComponent } from './switches/switches.component';
+
 
 const routes: Routes = [
   {
