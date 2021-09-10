601eda2dc9ab6c7508c9f4598f09789ad56e9828
Alejandro Pinedo B <apinedobarrios@live.com>
2021-09-10 14:57:59 -0500

    Se agregó el formulario template dinamico

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+  interface Persona{
+    nombre:string;
+    favoritos:Favorito[];
+  }
+  interface Favorito{
+    id:number;
+    nombre:string;
+  }
+
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +17,26 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent   {
+  @ViewChild('miFormulario') miFormulario!:NgForm; //Para hacer referencía a una propiedad local en nuestro HTML
 
-  constructor() { }
-
-  ngOnInit(): void {
+  persona: Persona={
+    nombre:'Alejandro',
+    favoritos:[
+      {id:1, nombre:'Halo'},
+      {id:2, nombre:'Doom'},
+      {id:3, nombre:'Fallout'},
+      {id:4, nombre:'TES'},
+      {id:5, nombre:'Guild Wars 2'},
+    ]
   }
 
+  nombreValido(){
+    return this.miFormulario?.controls.nombre?.errors&&this.miFormulario?.controls.nombre?.touched;
+  }
+
+  guardar(){
+    console.log('Formulario Posteado');
+    
+  }
 }
