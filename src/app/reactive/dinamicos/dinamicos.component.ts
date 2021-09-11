e0b17d4c0634e9b22e2022df89d65f29413029e0
Alejandro Pinedo B <apinedobarrios@live.com>
2021-09-11 18:34:45 -0500

    Se agregó el metodo para agregar favoritos

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validator, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-dinamicos',
@@ -10,30 +11,45 @@ import { FormArray, FormBuilder, FormGroup, Validator, Validators } from '@angul
 export class DinamicosComponent implements OnInit {
   miFormulario: FormGroup = this.fb.group(
     {
-      nombre: ['', [Validators.required, Validators.minLength(3)]],
-      favoritos:this.fb.array([
+      nombre: ['Alejandro', [Validators.required, Validators.minLength(3)]],
+      favoritos: this.fb.array([
         ['Metal Gear', Validators.required],
         ['Halo', Validators.required],
         ['Witcher', Validators.required],
         ['Fallout', Validators.required],
         ['Doom', Validators.required],
         ['God of War', Validators.required],
-        
+
         //[] es el resumen de la instrucción this.fb.control('',)
       ], Validators.required)
     }
   );
 
-    get favoritosArr(){
-      return this.miFormulario.get('favoritos') as FormArray;
-    }
+  nuevoFavorito: FormControl = this.fb.control('', Validators.required);//Control vease como un nuevo valor para el formulario
+
+  get favoritosArr() {
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
+
+
   campoIsValid(campo: string) {
     return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched;
+  }
+
+  agregarFavorito() {
+    if (this.nuevoFavorito.invalid) {
+      return;
+    }
+
+    // this.favoritosArr.push(new FormControl(this.nuevoFavorito.value,Validators.required)); //Es otra forma de setearle valores
+    this.favoritosArr.push(this.fb.control(this.nuevoFavorito.value,Validators.required));
+    this.nuevoFavorito.reset();
+
   }
 
   guardar() {
