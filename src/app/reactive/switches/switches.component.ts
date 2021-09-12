11838745dd3d0bd3c435dd70dc323cfe095fcdac
Alejandro Pinedo B <apinedobarrios@live.com>
2021-09-11 20:10:05 -0500

    Se agregarón los toques finales al switches para la validación

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -9,26 +9,46 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class SwitchesComponent implements OnInit {
 
-  miFormulario:FormGroup = this.fb.group({
-    genero: [ 'M', Validators.required ],
-    notificaciones: [ true, Validators.required ],
-    condiciones:[true,Validators.requiredTrue]
+  miFormulario: FormGroup = this.fb.group({
+    genero: ['M', Validators.required],
+    notificaciones: [true, Validators.required],
+    condiciones: [true, Validators.requiredTrue]
   });
 
-  
+
   persona = {
-    genero:'F',
-    notificaciones:false,
+    genero: 'F',
+    notificaciones: false,
   }
-  constructor( private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.miFormulario.reset(
-      {...this.persona, condiciones:false}
+      { ...this.persona, condiciones: false }
     );
+
+
+    this.miFormulario.valueChanges.subscribe(form => {
+      delete form.condiciones;
+      this.persona = form;
+    });
+
+    // Lo mismo de arriba pero usando destructuración en la ual extraeremos las condiciones del resto de argumentos
+    // this.miFormulario.valueChanges.subscribe(({condiciones,...rest}) => {
+    //   this.persona = rest;
+    // });
   }
-  
-  
+
+  guardar() {
+    const formValue = { ...this.miFormulario.value };
+    delete formValue.condiciones;
+
+    this.persona = formValue;
+
+
+  }
+
+
 
 
 }
