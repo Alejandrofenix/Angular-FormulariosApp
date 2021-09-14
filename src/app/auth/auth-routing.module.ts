d743c696e88f8d6bfcdcddce8f8b92523437423b
Alejandro Pinedo B <apinedobarrios@live.com>
2021-09-13 20:33:29 -0500

    Se creó la interfaz de las validaciones registro

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -7,8 +7,8 @@ const routes: Routes = [
   {
     path: '',
     children: [
-      { path: 'registro', component: RegistroComponent },
       { path: 'login', component: LoginComponent },
+      { path: 'registro', component: RegistroComponent },
       { path: '**', redirectTo: 'login' }
     ]
   }
